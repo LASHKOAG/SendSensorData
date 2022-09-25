218bba1e37982f42ee0b4a4d0c7cbdb407dfbca4
LASHKOAG <lashkoag@yandex.ru>
2022-09-25 11:19:12 +0600

    added function for init pins

--- getSensorData/getSensorData.ino
+++ getSensorData/getSensorData.ino
@@ -1,6 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+const int LED_BLUE = 6;
+const int LED_WHITE = 4;
+const int FOTORESISTOR_MAIN = A0;
 
+void InitPin(){
+  //Led for start-stop recive data
+  pinMode(LED_BLUE, OUTPUT);
+  
+  //Led for indicate process Get data from sensor (write array)
+  pinMode(LED_WHITE, OUTPUT);
+
+  //Pin for getting data from fotoresistor
+  pinMode(FOTORESISTOR_MAIN, INPUT);
+}
+
+void setup() {
+  InitPin();
 }
 
 void loop() {
